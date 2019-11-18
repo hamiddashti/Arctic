a46efb17b73a71116fa2acffedc8dd99b88276ab
hamiddashti <ahangar.hamid@gmail.com>
2019-11-18 08:39:28 -0700

    modis download

--- Modis/test.py
+++ Modis/test.py
@@ -4,10 +4,26 @@ Created on Thu Nov 14 10:08:04 2019
 
 @author: hamiddashti
 """
+"""
 from handy_funs import getTile 
 
 print ("UpperEast tile" + str(getTile(66.5078812,174.7286377)))
 print ("UpperWest tile" + str(getTile(73.0940475,-62.1090126)))
 print ("LowerEast tile" + str(getTile(48.6725121,-94.2617569)))
 print ("LowerWest tile" + str(getTile(45.3240967,-149.5862427)))
+"""
+
+import requests
+import getpass
+import time
+import os
+import cgi
+import json
+import pandas as pd
+import geopandas as gpd
+import xarray
+import numpy as np
+import hvplot.xarray
+import holoview as hv
+
 
